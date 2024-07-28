6e28542f216ea972ac50c4577123313ba7d3e47c
Arvin Verain <arvinverain@proton.me>
2024-07-29 00:39:54 +0800

    Remove redundant hlsearch option (#1058)

--- init.lua
+++ init.lua
@@ -83,8 +83,8 @@ vim.opt.tabstop = 2
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
