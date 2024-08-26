6a50efd533134068b59ec81c2518bdc23688fdfb
Bayram Kazik <48856944+bayramkzk@users.noreply.github.com>
2024-08-26 06:23:17 +0300

    Enable silent option for default neo-tree plugin keybinding (#1108)

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '<C-N>', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { '<C-N>', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
